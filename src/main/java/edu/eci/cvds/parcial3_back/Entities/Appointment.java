98b4f2dc541ae33f1e2082afb090dedca147200a
nicolas.toro-c <nikotoro25@gmail.com>
2025-05-23 17:02:33 -0500

    "Appointment"

--- src/main/java/edu/eci/cvds/parcial3_back/Entities/Appointment.java
+++ src/main/java/edu/eci/cvds/parcial3_back/Entities/Appointment.java
@@ -1,7 +1,46 @@
 package edu.eci.cvds.parcial3_back.Entities;
 
+import edu.eci.cvds.parcial3_back.Enum.Specialty;
 import org.springframework.boot.autoconfigure.graphql.ConditionalOnGraphQlSchema;
+
+import java.time.LocalDate;
 
 public class Appointment {
 
+    private LocalDate Date;
+    private Specialty Specialty;
+    private String Doctor;
+    private String Location;
+
+    public LocalDate getDate() {
+        return Date;
+    }
+
+    public void setDate(LocalDate date) {
+        Date = date;
+    }
+
+    public edu.eci.cvds.parcial3_back.Enum.Specialty getSpecialty() {
+        return Specialty;
+    }
+
+    public void setSpecialty(edu.eci.cvds.parcial3_back.Enum.Specialty specialty) {
+        Specialty = specialty;
+    }
+
+    public String getDoctor() {
+        return Doctor;
+    }
+
+    public void setDoctor(String doctor) {
+        Doctor = doctor;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
 }
--- src/main/java/edu/eci/cvds/parcial3_back/Repository/AppointmentRepository.java
+++ src/main/java/edu/eci/cvds/parcial3_back/Repository/AppointmentRepository.java
@@ -1,7 +1,8 @@
 package edu.eci.cvds.parcial3_back.Repository;
 
 import edu.eci.cvds.parcial3_back.Entities.Appointment;
+import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
 
-public interface AppointmentRepository{
+public interface AppointmentRepository extends MongoClientSettingsBuilderCustomizer {
 
 }
