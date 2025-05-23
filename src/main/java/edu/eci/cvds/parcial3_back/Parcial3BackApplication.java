8f8ac58609566d23821999868ed75140a1b38da2
unknown <nicolas.toro-c@labinfo.is.escuelaing.edu.co>
2025-05-23 15:16:25 -0500

    First commit

---
+++ src/main/java/edu/eci/cvds/parcial3_back/Parcial3BackApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.parcial3_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial3BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcial3BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/cvds/parcial3_back/Parcial3BackApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.parcial3_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial3BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
